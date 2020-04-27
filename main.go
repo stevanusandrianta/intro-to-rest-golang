864de1a8b76bc4c2b7305d663105de016d388ec5
Po Stevanus Andrianta <po.stevanus@thebonza.com>
2020-04-27 23:50:36 +0700

    implement update book

--- main.go
+++ main.go
@@ -94,11 +94,21 @@ func addBook(w http.ResponseWriter, r *http.Request) {
 	logFatal(err)
 
 	json.NewEncoder(w).Encode(bookID)
-
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
 
+	json.NewDecoder(r.Body).Decode(&book)
+
+	result, err := db.Exec("update books set title=$1, author=$2, year=$3 where id=$4 RETURNING id",
+		book.Title, book.Author, book.Year, book.ID)
+	logFatal(err)
+
+	rowsUpdated, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsUpdated)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
